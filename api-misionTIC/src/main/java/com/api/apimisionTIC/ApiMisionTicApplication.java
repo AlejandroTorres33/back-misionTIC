7f4a08416917918724e3585a0b4ea68b4962b77f
Alejandro Olarte Torres <jaot037@gmail.com>
2022-09-29 18:45:57 -0500

    Inicialización Back
    Se generó el proyecto Spring

---
+++ api-misionTIC/src/main/java/com/api/apimisionTIC/ApiMisionTicApplication.java
@@ -0,0 +1,13 @@
+package com.api.apimisionTIC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMisionTicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMisionTicApplication.class, args);
+	}
+
+}
